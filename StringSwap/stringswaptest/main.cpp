e2afe170c71d887a6b2b091667d9cdb693fb8a9d
scintil <Scintil89@gmail.com>
2017-06-13 07:24:04 +0900

    StringSwap - istringstream

---
+++ StringSwap/stringswaptest/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stack>
+
+int main()
+{
+	std::string str = "I got you in my sight";
+	//std::string res;
+	std::string token;
+	std::istringstream temp(str);
+	std::stack<std::string> tokStack;
+
+	//int i = 0;
+	//
+	//while (str[i] != '\0')
+	//{
+	//	std::cout << i << "¹øÂ° : " << str[i] << std::endl;
+	//	++i;
+	//}
+
+	//std::cout << str.size() << std::endl;
+
+	while (getline(temp, token, ' '))
+	{
+		std::cout << token << " - ";
+		tokStack.push(token);
+	}
+
+	std::cout << std::endl;
+
+	while (tokStack.empty() == false)
+	{
+		std::cout << tokStack.top() << " - ";
+		tokStack.pop();
+	}
+
+	getchar();
+
+	return 0;
+}
+
